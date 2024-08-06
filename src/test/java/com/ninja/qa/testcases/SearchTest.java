09030df1fcc919986a5584d74e88e371d0c4285a
morenitesh <more.nitesh871@gmail.com>
2024-08-07 00:42:38 +0530

    Added More Details

--- src/test/java/com/ninja/qa/testcases/SearchTest.java
+++ src/test/java/com/ninja/qa/testcases/SearchTest.java
@@ -11,7 +11,7 @@ import com.ninja.qa.base.baseClass;
 import com.ninja.qa.pages.HomePage;
 import com.ninja.qa.pages.SearchPage;
 
-//updated
+//updated -- Nitesh More (I Love you Babe)
 
 public class SearchTest extends baseClass {
 	SearchPage searchPage;
